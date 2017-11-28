6d81cb4fb1881b0d26b0a2f4bb40f1eb5f2b342c
balasv <bala.k.btech@gmail.com>
2017-11-28 12:12:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -77,9 +77,9 @@
 
 </select><br><br>
 
-<label for="ta">Write Your Code</label>
+<label for="ta">Type Your Code</label>
 <textarea class="form-control" name="code" rows="10" cols="50"></textarea><br><br>
-<label for="in">Enter Your Input</label>
+<label for="in">Enter Your Input(Optional)</label>
 <textarea class="form-control" name="input" rows="10" cols="50"></textarea><br><br>
 <input type="submit" class="btn btn-success" value="Run Code"><br><br><br>
 
